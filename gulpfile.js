201ea044321affa8d6d70fb2ad97e159d474908d
monstraaa <89292725827@mail.ru>
2022-11-10 17:40:45 +0500

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const {src,dest,series,watch} = require('gulp');
+const concat = require('gulp-concat');
+const del = require('del');
+const browserSync = require("browser-sync").create();
+
+// all ccs from src to one file in dist
+const styles = () => {
+    return src('src/style/**/*.css')
+        .pipe(concat('style.css'))
+        .pipe(dest('dist/style'))
+        .pipe(browserSync.stream());
+}
+
+const clean = () => {
+    return del('dist')
+}
+
+const watchFile = () => {
+    browserSync.init({
+        server:{
+            baseDir:'dist'
+        }
+    })
+}
+
+watch('src/style/**/*.css',styles);
+watch('stc/js/**/*.js',scripts);
+watch('src/resources/**',resources);
+
+exports.styles = styles;
+exports.clean = clean;
+exports.default = series(clean,styles,watchFile);
+//exports.default = series(clean,styles,htmlMinify,svgSprites,images,scripts,resources,watchFile);
