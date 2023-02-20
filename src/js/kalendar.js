4e10641277cd78fa29a98f3e7a92a6f7735adebb
AntonLogunov <toni.logunov.90@gmail.com>
2023-02-20 20:50:54 +0300

    Merge branch 'danil'

---
+++ src/js/kalendar.js
@@ -0,0 +1,30 @@
+const dateInput = document.getElementById("date");
+datepicker("#date", {
+  startDay: 1,
+  showAllDates: true,
+  disableMobile: true,
+  maxDate: new Date(2020, 4, 10),
+  minDate: new Date(2020, 0, 1),
+  dateSelected: new Date(2020, 0, 5),
+  overlayPlaceholder: "4-Stelliges Jahr",
+  customMonths: [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+  ],
+  customDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+  onSelect: (instance, date) => {
+    const value = date.toISOString().substring(0, 1);
+    console.log(date);
+    dateInput.value = value; // => '1/1/2099'
+  }
+});
