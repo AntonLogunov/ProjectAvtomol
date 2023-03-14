f0dcd3076307a5a6408427f91ef50cf451fdbccf
monstraaa <892922725827@amil.ru>
2023-03-14 12:23:59 +0500

    service fix all

--- src/js/vitaliy2.js
+++ src/js/vitaliy2.js
@@ -8,7 +8,15 @@ function createElementWithClass(elemType,className){
     elem.classList.add(className);
     return elem;
 }
-
+function gatherInf(service,valueP,valueT){
+    choiceInf.services.push(service["Name"]);
+    choiceInf.price += valueP;
+    choiceInf.time += valueT;
+    priceValue += valueP;
+    timeValue += valueT; 
+    price.innerHTML = priceValue;
+    time.innerHTML = timeValue;
+}
 let priceValue = 0;
 let timeValue = 0;
 const choiceInf = {
@@ -39,133 +47,110 @@ fetch('src/js/services.json')
             block.append(arrow);
             form.append(block);
 
+            let isMenuCreated = false;
+
             block.addEventListener("click",(event) => {
                 if(!event.currentTarget.classList.contains("section-form__active")){
                     event.currentTarget.querySelector(".section-form__arrow").innerHTML = "V";
                     event.currentTarget.classList.add("section-form__active");
-                    event.currentTarget.nextSibling.remove();
+                    event.currentTarget.nextSibling.classList.add("section-form-hidden");
                 }
                 else{
                     event.currentTarget.querySelector(".section-form__arrow").innerHTML = "^";
                     event.currentTarget.classList.remove("section-form__active");
+                    if(isMenuCreated == true){
+                        event.currentTarget.nextSibling.classList.remove("section-form-hidden");
+                    }
+                    else{
+                        isMenuCreated = true
+                        let menu = createElementWithClass("div","section-form__menu");
+                        let meniItems = createElementWithClass("div","section-form__menuItems");
 
-                    let menu = createElementWithClass("div","section-form__menu");
+                        let meniItemsArray = ["Наименовние работ","Отечественный","Иномарка","Время"];
+                        let firstItem = true;   
+                        meniItemsArray.forEach(itemName =>{
+                            let classForP = "section-form__menuItemName";
+                            if(firstItem) {classForP += "-first";}
+                            let item = createElementWithClass("p",classForP);
+                            firstItem = false;
+                            item.innerHTML = itemName;
+                            meniItems.append(item);
+                        })
+                        menu.append(meniItems);
 
-                    let meniItems = createElementWithClass("div","section-form__menuItems");
-
-                    let meniItemsArray = ["Наименовние работ","Отечественный","Иномарка","Время"];
-                    let firstItem = true;   
-                    meniItemsArray.forEach(itemName =>{
-                        let classForP = "section-form__menuItemName";
-                        if(firstItem) {classForP += "-first";}
-                        let item = createElementWithClass("p",classForP);
-                        firstItem = false;
-                        item.innerHTML = itemName;
-                        meniItems.append(item);
-                    })
-                    menu.append(meniItems);
-
-                    let items = createElementWithClass("div","section-form__items");
-                    nameGroup["service"].forEach(service => {
-                        let itemClass = (service.isActive == true)? "section-form__item-chosen":"section-form__item";//console.log(itemClass + "70");
-                        let item = createElementWithClass("button",itemClass);
-                        for(const txt in service){
-                            if(service[txt] != "+"){// remowes +
-                                let itemTxt = createElementWithClass("p","section-form__itemTxt");
-                                if(service[txt] == service["isActive"]){
-                                    continue;
-                                }
-                                if(service[txt] == service["NativeCar"] || service[txt] == service["ForeignCar"]){
-                                    itemTxt.innerHTML= service[txt] + " &#8381";
-                                }
-                                else{
-                                    itemTxt.innerHTML= service[txt];
-                                }
-                                item.append(itemTxt);
-                                const targetTxt = service["Name"];
-                            }
-                        }
-                        items.append(item);
-
-                        function eventListener() {
-                            item.classList = "section-form__item-chosen"; //console.log("chosen 91");
-                            service.isActive = true;
-                            let chosenItem = createElementWithClass("div","section-form__chosenItem");
-
-                            let chosenItemTxt = createElementWithClass("p","section-form__chosenItemTxt");
-                            chosenItemTxt.innerHTML = service["Name"];
-                            chosenItem.append(chosenItemTxt);
-                            let valueT = 0;
-                            if(service["LeadTime"].includes("час")){
-                                valueT = parseInt(service["LeadTime"].replace(/\D/g, ''))*60;
-                            }
-                            else if(service["LeadTime"].includes("мин")){
-                                valueT = parseInt(service["LeadTime"].replace(/\D/g, ''));
-                            }
-                            let valueP = 0;
-                            if(isNaN(service["NativeCar"])){
-                                valueP = parseInt(service["NativeCar"].replace(/\D/g, '')) + 0;
-                            }else{
-                                valueP = service["NativeCar"];
-                            }
-
-                            choiceInf.services.push(service["Name"]);
-                            choiceInf.price += valueP;
-                            choiceInf.time += valueT;
-
-                            priceValue += valueP;
-                            timeValue += valueT;
-                            price.innerHTML = priceValue;
-                            time.innerHTML = timeValue;
-
-                            let closeButton = createElementWithClass("button","section-form__closeButton");
-                            closeButton.innerHTML = "X";
-                            chosenItem.append(closeButton);
-                            closeButton.addEventListener("click",(event)=>{
-                                service.isActive = false;
-                                item.classList = "section-form__item"; //console.log("unchosen 126");
-
-                                let res = document.querySelectorAll(".section-form__item-chosen")
-                                //console.log(res);
-                                for (const chosenItem in res) {
-                                    if(isNaN(chosenItem)){
+                        let items = createElementWithClass("div","section-form__items");
+                        nameGroup["service"].forEach(service => {
+                            let itemClass = (service.isActive == true)? "section-form__item-chosen":"section-form__item";
+                            let item = createElementWithClass("button",itemClass);
+                            for(const txt in service){
+                                if(service[txt] != "+"){// remowes +
+                                    let itemTxt = createElementWithClass("p","section-form__itemTxt");
+                                    if(service[txt] == service["isActive"]){
                                         continue;
                                     }
-                                    if(res[chosenItem].firstChild.innerHTML == event.currentTarget.parentNode.firstChild.innerHTML){
-                                        res[chosenItem].classList = "section-form__item";
+                                    if(service[txt] == service["NativeCar"] || service[txt] == service["ForeignCar"]){
+                                        itemTxt.innerHTML= service[txt] + " &#8381";
                                     }
-                                    //console.log(res[chosenItem]);
-                                    //console.log(event.currentTarget.parentNode.firstChild.innerHTML)
+                                    else{
+                                        itemTxt.innerHTML= service[txt];
+                                    }
+                                    item.append(itemTxt);
+                                    const targetTxt = service["Name"];
                                 }
-                                
-                                event.currentTarget.parentNode.remove();
+                            }
+                            items.append(item);
+
+                            function eventListener() {
+                                item.classList = "section-form__item-chosen";
+                                service.isActive = true;
+                                let chosenItem = createElementWithClass("div","section-form__chosenItem");
+
+                                let chosenItemTxt = createElementWithClass("p","section-form__chosenItemTxt");
+                                chosenItemTxt.innerHTML = service["Name"];
+                                chosenItem.append(chosenItemTxt);
+                                let valueT = 0;
+                                if(service["LeadTime"].includes("час")){
+                                    valueT = parseInt(service["LeadTime"].replace(/\D/g, ''))*60;
+                                }
+                                else if(service["LeadTime"].includes("мин")){
+                                    valueT = parseInt(service["LeadTime"].replace(/\D/g, ''));
+                                }
+                                let valueP = 0;
+                                if(isNaN(service["NativeCar"])){
+                                    valueP = parseInt(service["NativeCar"].replace(/\D/g, '')) + 0;
+                                }else{
+                                    valueP = service["NativeCar"];
+                                }
+
+                                gatherInf(service,valueP,valueT);
+
+                                let closeButton = createElementWithClass("button","section-form__closeButton");
+                                closeButton.innerHTML = "X";
+                                chosenItem.append(closeButton);
+                                closeButton.addEventListener("click",(event)=>{
+                                    service.isActive = false;
+                                    item.classList = "section-form__item";
+
+                                    event.currentTarget.parentNode.remove();
+                                    addListener();
+
+                                    gatherInf(service,-valueP,-valueT);
+                                });
+                                form.append(chosenItem);
+                            }
+                            function addListener() {
+                                item.addEventListener("click", eventListener, {
+                                    passive: true,
+                                    once: true,
+                                });
+                            }
+                            if(!service.isActive){
                                 addListener();
-                                
-                                choiceInf.services.push(service["Name"]);
-                                choiceInf.price -= valueP;
-                                choiceInf.time -= valueT;
-
-                                priceValue -= valueP;
-                                timeValue -= valueT; 
-                                price.innerHTML = priceValue;
-                                time.innerHTML = timeValue;
-                                
-                            });
-
-                            form.append(chosenItem);
-                        }
-                        function addListener() {
-                            item.addEventListener("click", eventListener, {
-                                passive: true,
-                                once: true,
-                            });
-                        }
-                        if(!service.isActive){
-                            addListener();
-                        }
-                    });
-                    menu.append(items);
-                    insertAfter(event.currentTarget,menu);
+                            }
+                        });
+                        menu.append(items);
+                        insertAfter(event.currentTarget,menu);
+                    }
                 }
             });
         });
