5c0dd5eb77f1ead1e1641ad147b7cbf1d254b8a0
AntonLogunov <toni.logunov.90@gmail.com>
2023-04-14 15:48:53 +0300

    поправил стили у формы и добавил проверку формы

--- src/js/anton.js
+++ src/js/anton.js
@@ -1,29 +1,54 @@
 let castom = function () {
-    
-    let castomHeader = document.querySelectorAll('.castom__header');
-    let castomItem = document.querySelectorAll('.castom__time');
-
-    castomHeader.forEach(item => {
+    // ====================== КАТЕГОРИЯ =======================================
+    let castomHeaderCategory = document.querySelectorAll('.castom__header--category');
+    let castomItemCategory = document.querySelectorAll('.castom__time');
+    castomHeaderCategory.forEach(item => {
         item.addEventListener('click', ()=>{
             item.parentElement.classList.toggle('castom-is-active');
         });
         
     });
-    castomItem.forEach(item => {
+    castomItemCategory.forEach(item => {
         item.addEventListener('click', ()=>{
             let textTime = item.innerText, 
             castom = item.closest('.castom'),
-            currentText = castom.closest('.castom').querySelector('.castom__current'),
-
-            header = document.querySelector('.castom__header'),
-            headerID = document.getElementById('castom__header');
-            currentText.classList.add('castom__current--click');
-            header.classList.add("castom__header--click");
-            headerID.classList.add('castom__header--click');
-
+            currentText = castom.closest('.castom').querySelector('.castom__current');
         currentText.innerText = textTime;
         castom.classList.remove('castom-is-active');
         console.log(textTime);
+
+        headerCategory = document.getElementById('castom__header--category');
+        path = document.getElementById('path');
+        currentText.classList.add('castom__current--click');
+        headerCategory.classList.add('castom__header--click--category');
+        path.classList.add('path');
+
+    });
+    });
+
+    // =========================== ВРЕМЯ =======================================
+    let castomHeaderTime = document.querySelectorAll('.castom__header--time');
+    let castomItemTime = document.querySelectorAll('.castom__item--time');
+    castomHeaderTime.forEach(item => {
+        item.addEventListener('click', ()=>{
+            item.parentElement.classList.toggle('castom-is-active');
+        });
+        
+    });
+    castomItemTime.forEach(item => {
+        item.addEventListener('click', ()=>{
+            let textTime = item.innerText, 
+            castom = item.closest('.castom'),
+            currentText = castom.closest('.castom').querySelector('.castom__current--time');
+        currentText.innerText = textTime;
+        castom.classList.remove('castom-is-active');
+        console.log(textTime);
+
+        headerCategory = document.getElementById('castom__header--time');
+        path = document.getElementById('path--time');
+        currentText.classList.add('castom__current--click');
+        headerCategory.classList.add('castom__header--click--time');
+        path.classList.add('path');
 
     });
     });
