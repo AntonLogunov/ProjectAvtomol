3a343ba06c206f79a69a00c6a6d67561a293fe1b
rPo3HbIu-HoCoK <dyakonovdd@mail.ru>
2023-04-25 21:56:33 +0500

    Исправлено появление модального окна исправлен фон мод окна и добавлена тень блока

--- src/js/danilModal.js
+++ src/js/danilModal.js
@@ -1,11 +1,16 @@
 'use strict'
 
-const btnOpen = document.querySelector('btn--open');
-const modal = document.querySelector('modal');
+const btnOpen = document.querySelector('.btn--open');
+const modal = document.querySelector('.modal');
+
+const closeBtn = document.querySelector('.X');
 
 const modalViewHandler = () => {
     modal.classList.toggle('modal--open');
 }
 
+closeBtn.onclick = function() {
+    modal.classList.remove('modal--open');
+}
+
 btnOpen.addEventListener('click', modalViewHandler);
-modal.addEventListener('click', modalViewHandler);
