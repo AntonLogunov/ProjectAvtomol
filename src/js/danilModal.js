a85752de7c56469c5f42af076adb28738b045b48
rPo3HbIu-HoCoK <dyakonovdd@mail.ru>
2023-05-10 23:22:04 +0500

    Попытался исправить модальные окна с помощью изменения названий классов модальных окон

--- src/js/danilModal.js
+++ src/js/danilModal.js
@@ -1,16 +1,16 @@
 'use strict'
 
 const btnOpen = document.querySelectorAll('.btn--open');
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-sectionFive');
 
 const closeBtn = document.querySelector('.X');
 
 const modalViewHandler = () => {
-    modal.classList.toggle('modal--open');
+    modal.classList.toggle('modal--open-sectionFive');
 }
 
 closeBtn.onclick = function() {
-    modal.classList.remove('modal--open');
+    modal.classList.remove('modal--open-sectionFive');
 }
 
 btnOpen.forEach(btn => {
