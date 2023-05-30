a4519d01232df86d3fe095e59e7298498c851994
rPo3HbIu-HoCoK <dyakonovdd@mail.ru>
2023-05-30 12:57:35 +0500

    Адаптированы 1 5 9 секции

---
+++ src/js/burger.js
@@ -0,0 +1,7 @@
+'use strict'
+const menu = document.querySelector('.menuBurger');
+const burgerBtn = document.querySelector('.burger');
+
+burgerBtn.addEventListener('click', ()=>{
+    menu.classList.toggle('menuBurger--open')
+});
