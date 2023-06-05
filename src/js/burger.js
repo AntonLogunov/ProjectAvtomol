1adb3ff9cc136e31589e6b5898e931c239f922b8
monstraaa <89292725827@mail.ru>
2023-06-05 19:50:23 +0500

    styles fix for modal from burger

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,6 +2,27 @@
 const menu = document.querySelector('.menuBurger');
 const burgerBtn = document.querySelector('.burger');
 
+let btnopenBurger = document.querySelector('.menu__link__orange-burger');
+// let authorization = document.querySelector('.authorization');
+// let createAccount = document.querySelector('.createAccount');
+// let restoreAccount = document.querySelector('.restore');
+// let Alert = document.querySelector('.alert');
+// let input = document.querySelectorAll('.modal-input');
+
 burgerBtn.addEventListener('click', ()=>{
     menu.classList.toggle('menuBurger--open')
+    btnopenBurger.onclick = function() {
+        console.log(btnopen);
+        modal.style.display = "block";
+        authorization.style.display = "block";
+        createAccount.style.display = "none";
+        restoreAccount.style.display = "none";
+        Alert.style.display = "none";
+        input.forEach(function(inp){
+            inp.value = '';
+        })
+        eye.forEach(function(e){
+            e.src="src/img/openeye.svg";
+        })
+    }
 });
